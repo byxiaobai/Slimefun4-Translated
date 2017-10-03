e861b431cf482be10c00db096a9fbc6d73557ee9
StarWishsama <1552409060@qq.com>
2017-10-03 18:39:08 +0800

    无汉化（不需要汉化）

--- src/me/mrCookieSlime/Slimefun/AncientAltar/AltarRecipe.java
+++ src/me/mrCookieSlime/Slimefun/AncientAltar/AltarRecipe.java
@@ -1,7 +1,4 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) fieldsfirst 
-// Source File Name:   AltarRecipe.java
+//无需汉化
 
 package me.mrCookieSlime.Slimefun.AncientAltar;
 
