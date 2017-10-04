99ab382db34cddd108c4e3773c3e1e52a697e957
StarWishsama <1552409060@qq.com>
2017-10-04 11:30:12 +0800

    无汉化（不需要汉化）

--- src/me/mrCookieSlime/Slimefun/GEO/Resources/OilResource.java
+++ src/me/mrCookieSlime/Slimefun/GEO/Resources/OilResource.java
@@ -1,8 +1,3 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) fieldsfirst 
-// Source File Name:   OilResource.java
-
 package me.mrCookieSlime.Slimefun.GEO.Resources;
 
 import java.util.Random;
