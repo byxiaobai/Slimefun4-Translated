49094b34185b23266f5c02439af6b142045511c9
StarWishsama <1552409060@qq.com>
2017-10-04 11:25:28 +0800

    无汉化（不需要汉化）

--- src/me/mrCookieSlime/Slimefun/GEO/OreGenResource.java
+++ src/me/mrCookieSlime/Slimefun/GEO/OreGenResource.java
@@ -1,8 +1,3 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) fieldsfirst 
-// Source File Name:   OreGenResource.java
-
 package me.mrCookieSlime.Slimefun.GEO;
 
 import org.bukkit.block.Biome;
